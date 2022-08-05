5c7c323219afd0b0f6ba01c061bb1cc1b74dbe8d
TokcDK <TokcDK@Gmail.com>
2022-08-05 21:38:26 +0500

    test1 fill with leather armor data

---
+++ SynArmorAAFixMissingGenderWorldModel/PatcherSettings.cs
@@ -0,0 +1,28 @@
+﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynArmorAAFixMissingGenderWorldModel
+{
+    public class ArmorTemplate2
+    {
+        public FormLink<IKeywordGetter>? Keyword;
+        public Dictionary<BipedObjectFlag, FormLink<IArmorAddonGetter>?>? SlotArmorAddons;
+    }
+
+    public class PatcherSettings
+    {
+        [SynthesisSettingName("Ignore mods")]
+        [SynthesisTooltip("Excluded mods")]
+        // public string BaselineMod { get; set; } = "MyFacegenBaseline.esp";
+        public HashSet<ModKey> IgnoreMods = new();
+
+        [SynthesisTooltip("Data about keyword to detect and armor addons for slots")]
+        public HashSet<ArmorTemplate2> ArmorTemplateData = new HashSet<ArmorTemplate2>();
+    }
+}
